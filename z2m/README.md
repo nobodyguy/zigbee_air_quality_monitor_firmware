1dfd6c90dc82a3815362ddfd191562687f0158db
nobodyguy <4577368+nobodyguy@users.noreply.github.com>
2023-01-07 13:34:15 +0100

    Added Z2M external converter

---
+++ z2m/README.md
@@ -0,0 +1,7 @@
+# Zigbee air quality monitor Zigbee2MQTT integration
+Enable the external converter by adding the following to your Zigbee2MQTT `configuration.yaml`:
+```yaml
+external_converters:
+  - airQualityMonitor.js
+```
+More info: https://www.zigbee2mqtt.io/advanced/support-new-devices/01_support_new_devices.html
---
+++ z2m/airQualityMonitor.js
@@ -0,0 +1,19 @@
+const fz = require('zigbee-herdsman-converters/converters/fromZigbee');
+const tz = require('zigbee-herdsman-converters/converters/toZigbee');
+const exposes = require('zigbee-herdsman-converters/lib/exposes');
+const reporting = require('zigbee-herdsman-converters/lib/reporting');
+const extend = require('zigbee-herdsman-converters/lib/extend');
+const e = exposes.presets;
+const ea = exposes.access;
+
+const definition = {
+    zigbeeModel: ['AirQualityMonitor'],
+    model: 'AirQualityMonitor_v0.1',
+    vendor: 'DIY',
+    description: 'Air quality monitor',
+    fromZigbee: [fz.temperature, fz.humidity],
+    toZigbee: [], // Should be empty, unless device can be controlled (e.g. lights, switches).
+    exposes: [e.temperature(), e.humidity()],
+};
+
+module.exports = definition;
